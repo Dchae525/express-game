3ea6693448dc5854e5c227fe4a9493e2ab7a4beb
--global <danielchae08@gmail.com>
2020-03-01 16:06:24 -0600

    play with express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 300
+const path = require('path')
+
+app.get('/puppy', (req, res) => {
+    res.sendFile(path.resolve('index.jpg'))
+}).get('/hello/:name', (req, res) => {
+    res.send('hello  '+req.params.name.toLocaleUpperCase())
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
